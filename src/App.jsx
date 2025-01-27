88245723ba48b861accf730923a7be922d766034
Sohan1928 <sohanurrahman1928@gmail.com>
2025-01-27 21:46:05 +0600

    froggy web landing page start right now

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-red-400">
+        <h1 className="text-3xl font-bold text-center">
+          Froggy animated web page
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
